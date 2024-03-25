abf425e2e2f438b17a3e09b2924b414afddd729a
GJJMB <gabs5656@gmail.com>
2024-03-25 16:41:28 +0000

    Changed the navbar to be easier to copy paste
    added some logic that allows for the functionality
    of the navbar to be the same yet to be able to
    maintain the same navbar without all the
    verbosity of the code.
    
    Changes to be committed:
    	modified:   public_html/frontend/css/sidebar/sidebar.css
    	modified:   public_html/frontend/html/navbar/html_nav.php
    	modified:   public_html/pages/dashboard.php
    	modified:   public_html/pages/users.php

--- public_html/frontend/html/navbar/html_nav.php
+++ public_html/frontend/html/navbar/html_nav.php
@@ -1,7 +1,75 @@
 <?php
+include_once __DIR__."/../php_frontend_functions.php";
+include_once __DIR__."/../form/html_formgen.php";
+
+function defaults(){
+$profile = ("
+    <div class=\"profile-div\">
+        <p class=\"profile-username\">Username </p>
+        <div class=\"profile-dropdown\">
+            <p href=\"\" class=\"profile-dropdown-item\" id=\"profile-user-type\">
+                UserType
+            </p>
+            <a href=\"\" class=\"profile-dropdown-item\">Settings</a>
+            <a href=\"\" class=\"profile-dropdown-item\">Help</a>
+            <a href=\"\" class=\"profile-dropdown-item\" id=\"profile-logout-button\">
+                Sign out
+            </a>
+        </a>
+    </div>
+    ");
+
+$form_config = array(
+    "form_class" => "search-form",
+    "form_div_class" => "form-input-container",
+    "button_div_class" => "button-input-container",
+);
+
+$form_input_array = array(
+    "label_for" => array("input-search"),
+    "input_type" => array("text"),
+    "input_id" => array("input-search"),
+    "input_class" => array("search-bar"),
+    "input_required" => array("false"),
+    "input_maxlength" => array("50"),
+);
+
+$form_button_array = array(
+    "button_href" => array(""),
+    "button_class" => array("search-button"),
+    "button_type" => array("submit"),
+    "button_name" => array("submit"),
+    "button_value" => array("submit"),
+    "button_text" => array("search")
+);
+
+$form_inputs =  array(
+    "form_input" => make_form_inputs($form_input_array),
+    "button" => make_form_buttons($form_button_array),
+);
+
+$form_action = "";
+$form_method = "";
+
+$search = make_form(
+    $form_config,
+    $form_action,
+    $form_method,
+    $form_inputs
+);
+return array($search, $profile);
+}
+
 // This function generates a navigation bar with given configurations and links
-function make_navbar($navbar_content){
-    
+function make_navbar($input){
+    convert_to_array($input);
+    if($input == "default"){
+        $search = defaults()[0];
+        $profile = defaults()[1];
+        $navbar_content = array($search, $profile);
+    }else{
+        $navbar_content = $input;   
+    }
     // Starting to $ret .=  out the HTML for the navigation bar
     $ret = ("
     <nav class=\"navbar\">
--- public_html/pages/dashboard.php
+++ public_html/pages/dashboard.php
@@ -1,8 +1,6 @@
 <?php
 include_once __DIR__."/../frontend/html/html_head.php";
 include_once __DIR__."/../frontend/html/navbar/html_nav.php";
-include_once __DIR__."/../frontend/html/div_gen.php";
-include_once __DIR__."/../frontend/html/form/html_formgen.php";
 
 $language = "en";
 $page_name = "Dashboard";
@@ -10,60 +8,8 @@ $css_path = array("../frontend/css/pages/dashboard.css",
                   "../frontend/css/sidebar/sidebar.css",
                   "../frontend/css/navbar/navbar.css");
 $js_path = "";
-$profile = ("
-    <div class=\"profile-div\">
-        <p class=\"profile-username\">Username </p>
-        <div class=\"profile-dropdown\">
-            <p href=\"\" class=\"profile-dropdown-item\" id=\"profile-user-type\">
-                UserType
-            </p>
-            <a href=\"\" class=\"profile-dropdown-item\">Settings</a>
-            <a href=\"\" class=\"profile-dropdown-item\">Help</a>
-            <a href=\"\" class=\"profile-dropdown-item\" id=\"profile-logout-button\">
-                Sign out
-            </a>
-        </a>
-    </div>
-    ");
 
-$form_config = array(
-    "form_class" => "search-form",
-    "form_div_class" => "form-input-container",
-    "button_div_class" => "button-input-container",
-);
-
-$form_input_array = array(
-    "label_for" => array("input-search"),
-    "input_type" => array("text"),
-    "input_id" => array("input-search"),
-    "input_class" => array("search-bar"),
-    "input_required" => array("false"),
-    "input_maxlength" => array("50"),
-);
-
-$form_button_array = array(
-    "button_href" => array(""),
-    "button_class" => array("search-button"),
-    "button_type" => array("submit"),
-    "button_name" => array("submit"),
-    "button_value" => array("submit"),
-    "button_text" => array("search")
-);
-
-$form_inputs =  array(
-    "form_input" => make_form_inputs($form_input_array),
-    "button" => make_form_buttons($form_button_array),
-);
-
-$search = make_form(
-    $form_config,
-    $form_action,
-    $form_method,
-    $form_inputs
-);
-
-
-$navbar = array($search , $profile);
+$navbar = "default";
 make_head($language , $page_name , $css_path , $js_path);
 ?>
     <body>
